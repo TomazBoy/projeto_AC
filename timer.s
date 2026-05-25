63bd71102c9761cb9fa24831d6ca80105df94021
Tomás Antunes <tpantunes11@gmail.com>
2026-05-25 21:23:34 +0100

    switched from clock to timer, timer does R6/2 seconds ( ex: R6 = 2 counts 1 second)

--- timer.s
+++ timer.s
@@ -4,7 +4,7 @@
     .equ    TMR, 0x01
     .equ    TC, 0x02
     .equ    TIR, 0x03
-    .equ    pTC_COUNT, 0xFA
+    .equ    pTC_MAX_VAL, 0xFA ; 5ms of iterations 
 
     .text
     b   program
@@ -14,33 +14,45 @@ program:
     ldr sp, stack_top_addr
     b main
 
-main:; R4 = led_r R5 = led_g R6 = n/2 seconds counte
+main:; R4 = led_r R5 = led_g R6 = n/2 seconds counter
     mrs R0, CPSR ; move cpsr to r0
     mov R1, #0x10
     orr R0, R0, R1 ; enable interrupt bit 4
     msr CPSR, R0 ; save CPSR
-    mov R0, #TMR
+    mov R0, #TMR ; select TMR
     movt R0, #pTC_ADDRESS
-    mov R1, #pTC_COUNT
+    mov R1, #pTC_MAX_VAL
     str R1 , [R0, #0];stores match value in TMR
+    mov R6, #2 ; 1 second
+    mov R0, #TCR; ; select TCR
+    movt R0, #pTC_ADDRESS
+    mov R1, #1
+    str R1, [R0, #0] ; TCR on => MR on => counting reseted and paused
+    sub R1, R1, R1
+    str R1, [R0, #0] ; TCR off => MR off => counting reseted and restarted
 main_start:
+    mov R0, #0
+    cmp R0, R6
+    beq main_end
     b main_start
+main_end:
+    b   .
 
 isr:
-    mov R0, #TIR
+    mov R0, #TIR ; select TIR
     movt R0, #pTC_ADDRESS
     mov R1, #0
-    str R1 ,[R0, #0]
+    str R1 ,[R0, #0] ; every store gets write enable on wich resets TIR and stops interrupt request
     ldr R0, sw_counter
     mov R1, #0x64 ; 100 * 5ms = 0.5s
-    cmp R0, R1 ; if 0.5sec
+    cmp R0, R1 ; if 0.5s
     beq isr_end
-    add R0, R0, #1
+    add R0, R0, #1 ; if R0 != 100 R0++
     ldr R1, sw_addr
-    str R0, [R1, #0]
+    str R0, [R1, #0]; store sw_counter
     movs pc, lr
 isr_end:
-    add R6, R6, #1 ; .5 secs have passed
+    sub R6, R6, #1 ; .5 secs have passed
     mov R0, #0 
     str R0, [R1, #0] ; sw_counter back to zero
     movs pc, lr
